b4c7ac488bc9607e58bf9c1a6e3eb6d46c70612a
IdfbAn <lukazaridze4@gmail.com>
2024-02-28 19:13:25 +0400

    0 bytes XD

---
+++ 0bytesxd/README.md
@@ -0,0 +1,1 @@
+# [0 bytes XD](https://esolangs.org/wiki/0_bytes_XD)
---
+++ 0bytesxd/main.lua
@@ -0,0 +1,10 @@
+print("Enter code:")
+local code = io.read()
+
+if code == "0" then
+    print(0)
+elseif code == "1" then
+    while true do
+        print(1)
+    end
+end
